4ce3bf2e2115f3f4063df0faf99290479265ec99
Abhishek Chandrasekaran <abhishekc92@gmail.com>
2017-08-29 18:44:54 -0700

    add graph

--- app/src/main/java/com/example/dilip/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dilip/myapplication/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.dilip.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import static com.jjoe64.graphview.GridLabelRenderer.GridStyle.NONE;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -33,6 +42,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        //graph.setDrawBackground(true);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        //series.setDrawBackground(true);
+        //series.setBackgroundColor(Color.BLACK);
+        series.setColor(Color.GREEN);
+        GridLabelRenderer glr=graph.getGridLabelRenderer();
+        Viewport vp=graph.getViewport();
+        vp.setBackgroundColor(Color.BLACK);
+        glr.setGridColor(Color.WHITE);
+        glr.setGridStyle(NONE);
+        glr.setHorizontalAxisTitleColor(Color.WHITE);
+        glr.setVerticalAxisTitleColor(Color.WHITE);
+        glr.setHorizontalLabelsColor(Color.WHITE);
+        glr.setVerticalLabelsColor(Color.WHITE);
+        glr.setVerticalLabelsSecondScaleColor(Color.WHITE);
+        graph.addSeries(series);
         bt = (Button) findViewById(R.id.button);
         bt2 = (Button) findViewById(R.id.button2);
         bt.setOnClickListener(new View.OnClickListener() {
